13526669708d9c2de5aa70c2ddae72ff2f6151c9
nywooz <8634090+nywooz@users.noreply.github.com>
2018-04-10 16:09:53 +0100

    removed unwanted files

--- src/js/React DnD/01 Dustbin/Copy or Move/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Container from "./Container";
-
-export default class DustbinCopyOrMove extends Component {
-  render() {
-    return <Container {...this.props} />;
-  }
-}
